9d7b0f1c97707935cb6f972290fb14b9cec4ac28
Mathis003 <mathis.delsart@student.uclouvain.be>
2025-08-24 16:28:22 +0200

    Change Action Github

--- social-auto-poster.py
+++ social-auto-poster.py
@@ -52,14 +52,13 @@ def initialize_webdriver():
     chrome_options = Options()
     
     # Basic stealth options
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("--disable-web-security")
     chrome_options.add_argument("--allow-running-insecure-content")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-dev-shm-usage")
-
-    chrome_options.binary_location = "/usr/bin/chromium-browser"
     
     # Realistic user agent
     chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
