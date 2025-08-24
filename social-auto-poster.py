0f73317e54b10689cbca3ffb7ef9cad3a36a1cad
Mathis003 <mathis.delsart@student.uclouvain.be>
2025-08-24 16:24:14 +0200

    Change Action Github

--- social-auto-poster.py
+++ social-auto-poster.py
@@ -58,6 +58,8 @@ def initialize_webdriver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-dev-shm-usage")
+
+    chrome_options.binary_location = "/usr/bin/chromium-browser"
     
     # Realistic user agent
     chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
